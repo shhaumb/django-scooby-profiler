04f0c55984f2f84d5f00f14af522c14c2aa9bbfa
Shubham Jain <sj.iitr@gmail.com>
2018-09-16 03:44:28 +0530

    provide long_description in setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
 # setup the project
 setup(
     name="django-scooby-profiler",
-    version="2.0.1",
+    version="2.0.3",
     url="https://github.com/shhaumb/django-scooby-profiler",
     author="Shubham Jain",
     author_email="sj.iitr@gmail.com",
@@ -14,6 +20,8 @@ setup(
         "happening in app while serving a request, with the help of "
         "'Scooby profiler' chrome extension."
     ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords="django debugging profiler profiling sql tool",
     license="MIT",
     packages=find_packages(),
