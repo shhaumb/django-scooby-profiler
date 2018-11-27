9346d028bec919665d44e7676972b9c018d8356f
Shubham Jain <sj.iitr@gmail.com>
2018-11-27 19:50:19 +0530

    fix recursion in get_repr function

--- scooby/utils.py
+++ scooby/utils.py
@@ -31,7 +31,7 @@ def curate_filename(filename):
     return filename
 
 
-def get_repr(value):
+def get_repr(value, nest_level=0):
     try:
         unicode_cls = unicode
     except NameError:
@@ -59,14 +59,17 @@ def get_repr(value):
             value.__class__.__name__,
             get_repr(value.pk),
         )
-    elif isinstance(value, tuple):
-        return "(%s)" % (', '.join([get_repr(item) for item in value]))
-    elif isinstance(value, list):
-        return "[%s]" % (', '.join([get_repr(item) for item in value]))
-    elif isinstance(value, dict):
-        return "{%s}" % (', '.join([
-            '%s: %s' % (get_repr(key), get_repr(value)) for (key, value) in
-            value.items()]))
+    elif nest_level == 0 and isinstance(value, (tuple, list, dict)):
+        if isinstance(value, tuple):
+            return "(%s)" % (', '.join(
+                [get_repr(item, nest_level+1) for item in value]))
+        elif isinstance(value, list):
+            return "[%s]" % (', '.join(
+                [get_repr(item, nest_level+1) for item in value]))
+        elif isinstance(value, dict):
+            return "{%s}" % (', '.join([
+                '%s: %s' % (get_repr(key), get_repr(value, nest_level+1))
+                for (key, value) in value.items()]))
     else:
         try:
             return "<%s>" % (value.__class__.__name__)
